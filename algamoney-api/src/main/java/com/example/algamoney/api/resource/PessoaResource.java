f5453f499e2754bd43a8fa99e32f41dbc3f4d4ab
Xilipe <Xilipe@DESKTOP-DD9FQC5>
2023-04-30 18:29:34 -0300

    Created Push methods in Pessoa

--- algamoney-api/src/main/java/com/example/algamoney/api/resource/PessoaResource.java
+++ algamoney-api/src/main/java/com/example/algamoney/api/resource/PessoaResource.java
@@ -4,13 +4,17 @@ import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -20,6 +24,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.example.algamoney.api.event.RecursoCriadoEvent;
 import com.example.algamoney.api.model.Pessoa;
 import com.example.algamoney.api.repository.PessoaRepository;
+import com.example.algamoney.api.service.PessoaService;
 
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
@@ -30,6 +35,10 @@ public class PessoaResource {
 
     @Autowired
     private PessoaRepository pessoaRepository;
+    
+    @Autowired
+    private PessoaService pessoaService;
+        
     
     @Autowired
     private ApplicationEventPublisher publisher;
@@ -49,4 +58,23 @@ public class PessoaResource {
         return pessoa.isPresent() ? ResponseEntity.ok(pessoa.get()) : ResponseEntity.notFound().build();
     }
     
+    @DeleteMapping("/{codigo}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void remover(@PathVariable Long codigo) {
+    	this.pessoaRepository.deleteById(codigo);
+    }
+    
+    @PutMapping("/{codigo}")
+    public Pessoa atualizar(@PathVariable Long codigo, @Valid @RequestBody Pessoa pessoa){
+    	Pessoa pessoaSalva = pessoaService.atualizar(codigo, pessoa);
+    	
+    	
+    	return this.pessoaRepository.save(pessoaSalva);
+    }
+    
+    @PutMapping("/{codigo}/ativo")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void atualizarPropriedadeAtivo(@PathVariable Long codigo, @RequestBody Boolean ativo) {
+    	pessoaService.atualizarPropriedadeAtivo(codigo, ativo);
+    }
 }
